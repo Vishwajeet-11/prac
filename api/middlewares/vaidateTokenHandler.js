5e5f59e16c53575eea374d7195d51165066a4fed
Vishwajeet-11 <vishwajeetbharadiya12@gmail.com>
2024-02-26 18:01:02 +0530

    created jwt auth middleware(validTokenHandler.js) for route protection, not working though, got to figure that out and merge the test branch to main

---
+++ api/middlewares/vaidateTokenHandler.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const validateToken = asyncHandler(async (req, res, next) => {
+  let token;
+  let authHeader = req.headers.authorization || req.headers.Authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(400);
+        throw new Error("user not Autorized");
+      }
+      req.user = decoded.user;
+      next();
+    });
+    if(!token){
+        res.status(400)
+        throw new Error("user not authorized or token is missing")
+    }
+  }
+});
+
+module.exports = validateToken
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express= require("express")
 const router = express.Router()
 const {userRegister, userLogin, allUsers} = require("../controllers/userController")
+const validateToken = require("../middlewares/vaidateTokenHandler")
 
 router.post("/api/users/register", userRegister)
-router.get("/api/users", allUsers)
+router.get("/api/users",validateToken, allUsers)
 router.post("/api/users/login", userLogin)
 
 
